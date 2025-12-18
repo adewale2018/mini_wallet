772aef6e9319902c8c1798c5c2c635721a07a105
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2025-12-18 15:30:10 +0100

    Setup the project with Vite-TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-medium text-center mt-10 text-orange-600 font-serif">
+        Mini Wallet Project Setup
+      </h1>
+    </div>
+  );
+}
+
+export default App;
